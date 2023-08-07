d5d4ca13f9a771306ceef7c0b69a97e723b71aa4
mateus <imw.desenv@gmail.com>
2023-08-07 20:35:25 -0300

    Carpool.DAL layers

---
+++ Carpool.DAL/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Carpool.DAL
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddSingleton<IConnectionMultiplexer>(opt => ConnectionMultiplexer.Connect())
+
+            return services;
+        }
+    }
+}
---
+++ Carpool.DAL/Persistence/Redis/Interfaces/IRideRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Carpool.DAL.Persistence.Redis.Interfaces
+{
+    public interface IRideRepository
+    {
+        Task CreateRideRequest();
+
+        Task DeleteRideRequest();
+    }
+}
