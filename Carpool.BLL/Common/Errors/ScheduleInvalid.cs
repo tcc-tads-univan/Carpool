f4442ab9b78c4155b051265c88d7c1a8fae40c4d
mateus <imw.desenv@gmail.com>
2023-08-19 12:57:33 -0300

    fix sheduleInvalid error message

--- Carpool.BLL/Common/Errors/ScheduleInvalid.cs
+++ Carpool.BLL/Common/Errors/ScheduleInvalid.cs
@@ -6,7 +6,7 @@ namespace Carpool.BLL.Common.Errors
     {
         public List<IError> Reasons => new List<IError>();
 
-        public string Message => "The schedule dosn't exist or has already been changed";
+        public string Message => "The schedule doesn't exist or has already been changed";
 
         public Dictionary<string, object> Metadata => new Dictionary<string, object>()
         {
