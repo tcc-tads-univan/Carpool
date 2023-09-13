3d0d8b324f81e45699dad9e61aa6b1738f296090
mateus <imw.desenv@gmail.com>
2023-09-13 00:52:34 -0300

    feat: Add json map

--- Carpool.DAL/Infrastructure/Services/Driver/Model/Driver.cs
+++ Carpool.DAL/Infrastructure/Services/Driver/Model/Driver.cs
@@ -1,11 +1,22 @@
-﻿namespace Carpool.DAL.Infrastructure.Services.Driver.Model
+﻿using System.Text.Json.Serialization;
+
+namespace Carpool.DAL.Infrastructure.Services.Driver.Model
 {
     public class Driver
     {
+        [JsonPropertyName("name")]
         public String Name { get; set; }
+
+        [JsonPropertyName("photoUrl")]
         public String PhotoUrl { get; set; }
+
+        [JsonPropertyName("rating")]
         public decimal Rating { get; set; }
+
+        [JsonPropertyName("phoneNumber")]
         public String PhoneNumber { get; set; }
+
+        [JsonPropertyName("vehiclePlate")]
         public String VehiclePlate { get; set; }
     }
 }
